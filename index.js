684a14b35cbe5895cfe985a8a9a585ecbd731c14
Aris Vayas <aris.vayas@gmail.com>
2022-04-05 13:52:50 -0400

    commit tree bfb9b2534096c4ef8cad87ad3079f673ad5c5132

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+let newArray=[]
+
+function writeCards(messages,events){
+    
+for ( let i =0; i < messages.length; i++)
+{
+    let newMessage =`Thank you, ${messages[i]}, for the wonderful ${events} gift!`
+ 
+ newArray.push(newMessage)
+}
+return newArray
+
+}
+
+function countDown(integerGiven){
+ let i = integerGiven
+ while (i >= 0) {
+     console.log(i--)
+ }
+}
